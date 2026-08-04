33a99dc0156bf9830bd6fb232f023b8c3703e539
Joyce Radis de Souza <255958806+joyceradis@users.noreply.github.com>
2026-08-04 11:43:08 -0300

    install central dialog controller

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 import { createStore } from './core/store.js';
 import { createApp } from './ui/app.js';
+import { installDialogController } from './ui/dialog-controller.js';
 
 const root=document.querySelector('#app');
 const toast=document.querySelector('#toast');
 const store=createStore();
+
+installDialogController(document);
 createApp({store,root,toast});
 
 if('serviceWorker' in navigator){
