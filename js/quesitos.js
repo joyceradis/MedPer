f88020b34b42c04e525553dc832d70a91f68225f
Joyce Radis de Souza <255958806+joyceradis@users.noreply.github.com>
2026-08-08 18:37:54 -0300

    chore: remove orphaned MLKS runtime

--- js/quesitos.js
+++
@@ -1,4 +0,0 @@
-window.MLKSQuesitos = {
-  classifications:['Respondível','Parcialmente respondível','Prejudicado por falta de elementos','Extrapola o objeto','Matéria jurídica','Fora da competência médica'],
-  statuses:['Pendente','Em elaboração','Respondido','Revisado']
-};
